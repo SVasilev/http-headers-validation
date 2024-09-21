0e8594fd6042efee5482d0c0adec2d665ad28b94
IgorBezanovic <igorbezanovic@gmail.com>
2024-09-21 20:02:49 +0200

    feat: Added JSDocs for TypeScript support

--- index.js
+++ index.js
@@ -2,10 +2,23 @@
 
 'use strict';
 
+/**
+ * Checks if the given value is a string.
+ * 
+ * @param {*} value - The value to check.
+ * @returns {boolean} `true` if the value is a string, otherwise `false`.
+ */
 function isString(value) {
   return Object.prototype.toString.call(value) === '[object String]';
 }
 
+/**
+ * Generates an array containing a range of numbers.
+ * 
+ * @param {number} start - The starting number (inclusive).
+ * @param {number} end - The ending number (exclusive).
+ * @returns {number[]} An array containing numbers from `start` to `end - 1`.
+ */
 function range(start, end) {
   var rangeResult = [];
   for (var i = start; i < end; i++) {
@@ -14,12 +27,29 @@ function range(start, end) {
   return rangeResult;
 }
 
+/**
+ * Checks if a value is either not a string or has zero length.
+ * 
+ * @param {*} value - The value to check.
+ * @returns {boolean} `true` if the value is not a string or has zero length, otherwise `false`.
+ */
 function invalidTypeOrLength(value) {
   return !isString(value) || value.length === 0;
 }
 
 // Inspired by Node.js: https://github.com/nodejs/node/blob/master/lib/_http_outgoing.js
+/**
+ * An API for validating HTTP header names and values.
+ * 
+ * @namespace httpHeaderValidationAPI
+ */
 var httpHeaderValidationAPI = {
+  /**
+   * Validates the HTTP header name.
+   * 
+   * @param {string} headerName - The name of the HTTP header to validate.
+   * @returns {boolean} `true` if the header name is valid, otherwise `false`.
+   */
   validateHeaderName: function(headerName) {
     if (invalidTypeOrLength(headerName)) {
       return false;
@@ -39,6 +69,12 @@ var httpHeaderValidationAPI = {
     });
   },
 
+  /**
+   * Validates the HTTP header value.
+   * 
+   * @param {string} headerValue - The value of the HTTP header to validate.
+   * @returns {boolean} `true` if the header value is valid, otherwise `false`.
+   */
   validateHeaderValue: function(headerValue) {
     if (invalidTypeOrLength(headerValue)) {
       return false;
@@ -51,6 +87,13 @@ var httpHeaderValidationAPI = {
     });
   },
 
+  /**
+   * Validates the HTTP header name and value pair.
+   * 
+   * @param {string} name - The name of the HTTP header to validate.
+   * @param {string} value - The value of the HTTP header to validate.
+   * @returns {boolean} `true` if both the header name and value are valid, otherwise `false`.
+   */
   validateHeader: function(name, value) {
     if (invalidTypeOrLength(name) || invalidTypeOrLength(value)) {
       return false;
